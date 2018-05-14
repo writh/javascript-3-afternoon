efef4b4fcd9563747864058b512c5b6cfb21ef73
Clifford Christensen <victoria.thursday@gmail.com>
2018-05-14 08:11:57 -0600

    finished higher-order.js

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,8 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+
+let evenNumbers = mixedNumbers.filter(element => element % 2 === 0)
 
 
 
@@ -44,7 +44,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(element => element * 1.07);
 
 
 
@@ -63,7 +63,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(result, item){
+  return result + item;
+})
 
 
 
@@ -89,7 +91,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(element => element["CP"] > 200);
 
 
 
@@ -106,7 +108,7 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get the sum of all the order totals after adding in the sales tax
 */
 
-let ordersTotal //Code Here
+let ordersTotal = orders.map( element => element * orders.tax)
 
 
 
@@ -126,6 +128,13 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce(function(count, element, index, arr) {
+  if (arr[index]["owner"] === "Bob") {
+    return count + arr[index]["price"];
+  }
+  else {
+    return count;
+  }
+}, 0);
 
 
